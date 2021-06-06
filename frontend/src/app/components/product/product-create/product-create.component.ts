3621148b267ac2ee23360c322aa1a2bf3b72dae4
JoseaneGrigolo <joseaneig@gmail.com>
2021-06-06 20:35:11 -0300

    agora vai

---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Router } from '@angular/router';
+import { Product } from '../product.model';
+
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: 'Produto de Teste',
+    price: 125.98
+  }
+
+
+constructor(private productService: ProductService,
+  private router: Router) { }
+
+ngOnInit(): void {
+
+}
+
+createProduct(): void {
+  this.productService.create(this.product).subscribe(()=>{
+    this.productService.showMessage('Produto criado!');
+    this.router.navigate(['/products']);
+  });
+}
+
+cancel(): void {
+  this.router.navigate(['/products']);
+}
+
+}
+
+
